f8df1e6d1224a5cd63c95a571e46794294499816
Xaliljon <kh.bakhtiyorov@yt.uz>
2021-10-26 15:51:27 +0500

    - 8 solution added

--- src/uz/peachdev/uz/leetcode/GoalParserInterpretation.java
+++ src/uz/peachdev/uz/leetcode/GoalParserInterpretation.java
@@ -3,10 +3,24 @@ package uz.peachdev.uz.leetcode;
 public class GoalParserInterpretation {
     class Solution {
         public String interpret(String command) {
-            String a = command.replace("()","o");
-            String b = a.replace("G","G");
-            String c = b.replace("(al)","al");
-            return c;
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < command.length(); i++) {
+                char ch = command.charAt(i);
+                int next = i + 1;
+                if (ch == '(' || ch == 'a') {
+                    if (command.charAt(next) == 'a') {
+                        res.append("a");
+                    } else if (command.charAt(next) == ')') {
+                        res.append("o");
+                    } else if (command.charAt(next) == 'l') {
+                        res.append(command.charAt(next));
+                    }
+                } else {
+                    if(ch == 'G')
+                        res.append(ch);
+                }
+            }
+            return res.toString();
         }
     }
 }
